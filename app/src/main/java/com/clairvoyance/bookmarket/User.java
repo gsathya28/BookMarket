1541fcac218d0f51a2c45a79f877881e324b7a32
Sathyanarayanan Govindarajan <gsathya28@gmail.com>
2017-12-21 20:52:50 -0600

    Initial commit

---
+++ app/src/main/java/com/clairvoyance/bookmarket/User.java
@@ -0,0 +1,21 @@
+package com.clairvoyance.bookmarket;
+
+/**
+ * Created by Sathya on 12/21/2017.
+ */
+
+public class User {
+    private String name;
+    private double UID;
+    private String email;
+
+    public User(String name, double UID, String email){
+        this.name = name;
+        this.UID = UID;
+        this.email = email;
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/clairvoyance/bookmarket/WebServiceHandler.java
@@ -0,0 +1,20 @@
+package com.clairvoyance.bookmarket;
+
+import android.content.Context;
+
+/**
+ * Created by Sathya on 12/21/2017.
+ */
+
+public class WebServiceHandler {
+
+    public WebServiceHandler(Context context){
+
+    }
+
+    public User generateUser(){
+
+        return new User("", 5, "");
+    }
+
+}
