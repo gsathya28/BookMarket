2bc5982ca6555dcb76d415846715cd67f3ee2bca
Sathyanarayanan Govindarajan <gsathya28@gmail.com>
2017-12-26 11:41:23 -0600

    Commit before implementing AWS DDB.

--- app/src/main/java/com/clairvoyance/bookmarket/WebServiceHandler.java
+++ app/src/main/java/com/clairvoyance/bookmarket/WebServiceHandler.java
@@ -1,5 +1,10 @@
 package com.clairvoyance.bookmarket;
 
+import android.content.Context;
+
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.regions.Regions;
+
 import java.util.ArrayList;
 
 /**
@@ -9,12 +14,20 @@ import java.util.ArrayList;
 
 class WebServiceHandler {
 
-    static double mainUID;
+    private static double mainUID;
 
-    static boolean authenticateMainUser(User localUser){
+
+    static boolean authenticateMainUser(User localUser, Context context){
 
         // Use AWS to get main user credentials and authenticate
         // Test mainUID
+
+        // Initialize the Amazon Cognito credentials provider
+        CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
+                context,
+                "us-east-1:08f4fe45-b937-45d2-b943-c6690c4fe3c7", // Identity pool ID
+                Regions.US_EAST_1 // Region
+        );
 
         return false;
     }
