16357322957cf43c1db32c07a732216b0cff7256
Sathyanarayanan Govindarajan <gsathya28@gmail.com>
2018-01-05 16:29:35 -0600

    Add Toolbar to Sell-Main Activity

--- app/src/main/java/com/clairvoyance/bookmarket/ActSellMainActivity.java
+++ app/src/main/java/com/clairvoyance/bookmarket/ActSellMainActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -30,9 +33,39 @@ public class ActSellMainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sell_main);
 
         mainUser = LocalDataHandler.parseMainUserData(getApplicationContext());
-
+        setToolbar();
         setOptionButtons();
         setLayout();
+    }
+
+    private void setToolbar(){
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        myToolbar.setTitleTextColor(Color.parseColor("#FFFFFF"));
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.sell_nav, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.action_search_event:
+                // This will lead to an activity (GUI) that will Search/Scan
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void setOptionButtons(){
