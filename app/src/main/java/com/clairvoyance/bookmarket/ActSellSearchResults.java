4adf67dbd13d3a559e9e3f16a65499932ca2fd30
Sathyanarayanan Govindarajan <gsathya28@gmail.com>
2018-01-05 16:46:29 -0600

    Build GUIs - Search/Scan Results GUI

--- app/src/main/java/com/clairvoyance/bookmarket/ActSellSearchResults.java
+++ app/src/main/java/com/clairvoyance/bookmarket/ActSellSearchResults.java
@@ -3,11 +3,11 @@ package com.clairvoyance.bookmarket;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SellSearchScan extends AppCompatActivity {
+public class ActSellSearchResults extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sell_search_scan);
+        setContentView(R.layout.activity_sell_search_results);
     }
 }
