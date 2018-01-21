fd311ca2e121469c05c16aae0002366fa875318c
Sathyanarayanan Govindarajan <gsathya28@gmail.com>
2018-01-21 11:35:27 -0600

    Addition of ViewRequestsFile - Last STABLE VERSION!

---
+++ app/src/main/java/com/clairvoyance/bookmarket/ActSellViewRequests.java
@@ -0,0 +1,13 @@
+package com.clairvoyance.bookmarket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActSellViewRequests extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sell_view_requests);
+    }
+}
