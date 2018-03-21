6d7c7c8628ddcc881ce556cca5b24550875d0e37
Sathyanarayanan Govindarajan <gsathya28@gmail.com>
2018-03-21 12:55:11 -0500

    Firebase Messaging Classes
    -Firebase ID Service (Token Methods)
    -Firebase Messaging service (pushes notifis when message is received)

--- app/src/main/java/com/clairvoyance/bookmarket/MyFirebaseInstanceIDService.java
+++ app/src/main/java/com/clairvoyance/bookmarket/MyFirebaseInstanceIDService.java
@@ -1,5 +1,8 @@
 package com.clairvoyance.bookmarket;
 
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
 /**
@@ -9,4 +12,17 @@ import com.google.firebase.iid.FirebaseInstanceIdService;
 
 public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
 
+    private static final String TAG = "MyAndroidFCMIIDService";
+
+    @Override
+    public void onTokenRefresh() {
+        //Get hold of the registration token
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        //Log the token
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+    }
+    private void sendRegistrationToServer(String token) {
+        //Implement this method if you want to store the token on your server
+    }
+    
 }
--- app/src/main/java/com/clairvoyance/bookmarket/MyFirebaseMessagingService.java
+++ app/src/main/java/com/clairvoyance/bookmarket/MyFirebaseMessagingService.java
@@ -1,6 +1,18 @@
 package com.clairvoyance.bookmarket;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
 /**
  * Created by Sathya on 1/24/2018.
@@ -9,5 +21,54 @@ import com.google.firebase.messaging.FirebaseMessagingService;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
+    private final static String TAG = "MyAndroidFCMService";
+    private final static String CHANNEL_ID = "channelID";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        //Log data to Log Cat
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+        //create notification
+        createNotification(remoteMessage.getNotification().getBody());
+    }
+
+    private void createNotification( String messageBody) {
+        Intent intent = new Intent( this , ActSellMainActivity. class );
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent resultIntent = PendingIntent.getActivity( this , 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri notificationSoundURI = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationManager notificationManager = (NotificationManager) (this.getSystemService(Context.NOTIFICATION_SERVICE));
+
+        NotificationCompat.Builder mNotificationBuilder = new NotificationCompat.Builder( this, CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Android Tutorial Point FCM Tutorial")
+                .setContentText(messageBody)
+                .setAutoCancel( true )
+                .setSound(notificationSoundURI)
+                .setContentIntent(resultIntent);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create the NotificationChannel, but only on API 26+ because
+            // the NotificationChannel class is new and not in the support library
+            CharSequence name = "Example Channel Name";
+            String description = "Example Channel Description";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system
+            if (notificationManager != null) {
+                notificationManager.createNotificationChannel(channel);
+            }
+        }
+
+        if (notificationManager != null) {
+            notificationManager.notify(0, mNotificationBuilder.build());
+        }
+
+    }
 
 }
